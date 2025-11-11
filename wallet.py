37d309f0926929b06ceee7409da6e3bdf24f1faa
GorkaRuiz9 <gorkaruiz2004@gmail.com>
2025-11-11 16:35:14 +0100

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -3,10 +3,9 @@ class Wallet:
         self.balance = balance
 
     def set_balance(self, val):
-        self.balance = self.balance + val + 777777777777777777777777777777777777777777777777777777777777777777777777777777777
-
+        self.balance = self.balance + val
     def get_balance(self):
         return self.balance
 
     def remove_balance(self, val):
-        self.balance = self.balance - val - 77777777777777777777777777777777777777777777777777777777777777777777777777777777777
+        self.balance = self.balance - val
